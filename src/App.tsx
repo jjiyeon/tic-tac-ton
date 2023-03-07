18eaa6e1f9447d081f5d68b4163837ac56aaad93
jjiyeon <ejy1105@gmail.com>
2023-03-08 02:33:05 +0900

    style: 안쓰는 파일 및 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import "./App.css";
-import { TonConnectButton } from "@tonconnect/ui-react";
-import { Counter } from "./components/Counter";
-import { Jetton } from "./components/Jetton";
-import { TransferTon } from "./components/TransferTon";
-import styled from "styled-components";
-import { Button, FlexBoxCol, FlexBoxRow } from "./components/styled/styled";
-import { useTonConnect } from "./hooks/useTonConnect";
-import { CHAIN } from "@tonconnect/protocol";
-import "@twa-dev/sdk";
-import TicTacToe from "./components";
-import * as UI from "./components/style";
+import './App.css'
+import { TonConnectButton } from '@tonconnect/ui-react'
+import '@twa-dev/sdk'
+import { useTonConnect } from './hooks/useTonConnect'
+import styled from 'styled-components'
+import TicTacToe from './components'
+import * as UI from './components/style'
 
 const StyledApp = styled.div`
   background-color: #2eaddc;
-  background-image: radial-gradient(
-    149.59% 88.94% at -53.99% -42.96%,
-    rgba(249, 69, 141, 0) 0%,
-    rgba(249, 69, 141, 0) 0.01%,
-    #201e45 100%
-  );
+  background-image: radial-gradient(149.59% 88.94% at -53.99% -42.96%, rgba(249, 69, 141, 0) 0%, rgba(249, 69, 141, 0) 0.01%, #201e45 100%);
   width: 100%;
   height: 100vh;
   color: black;
@@ -27,9 +17,7 @@ const StyledApp = styled.div`
     background-color: #222;
     color: white;
   }
-  /* min-height: 100vh; */
-  /* padding: 20px 20px; */
-`;
+`
 
 const AppContainer = styled.div`
   display: flex;
@@ -39,10 +27,10 @@ const AppContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   gap: 20px;
-`;
+`
 
 function App() {
-  const { network } = useTonConnect();
+  const { network } = useTonConnect()
 
   return (
     <StyledApp>
@@ -51,24 +39,9 @@ function App() {
           <TonConnectButton />
         </UI.ConnectWallet>
         <TicTacToe />
-        {/* <FlexBoxCol>
-          <FlexBoxRow>
-            <TonConnectButton />
-            <Button>
-              {network
-                ? network === CHAIN.MAINNET
-                  ? "mainnet"
-                  : "testnet"
-                : "N/A"}
-            </Button>
-          </FlexBoxRow>
-          <Counter />
-          <TransferTon />
-          <Jetton />
-        </FlexBoxCol> */}
       </AppContainer>
     </StyledApp>
-  );
+  )
 }
 
-export default App;
+export default App
