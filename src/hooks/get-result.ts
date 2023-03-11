be4cad1357e13cb21388c6e55abefb4c5f66dbb4
jjiyeon <ejy1105@gmail.com>
2023-03-12 05:18:20 +0900

    feat: 스티븐 예제코드

--- src/hooks/get-result.ts
+++ src/hooks/get-result.ts
@@ -1,23 +1,27 @@
 /** @format */
 
 import { getHttpEndpoint } from '@orbs-network/ton-access'
-import { TonClient, Address } from 'ton'
-import Game from '../libs/game'
+import { TonClient, Address, Sender } from 'ton'
+import Game from '../contract/game'
+import { useTonClient } from './useTonClient'
 
 /**
  * 결과값을 가지고 오는 함수,
  * result[]만 사용하면 될듯
  */
-export async function main() {
-  const endpoint = await getHttpEndpoint({ network: 'mainnet' })
-  const client = new TonClient({ endpoint })
-
+export async function getMain({ sender, walletAddr, client }: { sender: Sender; walletAddr: string; client: TonClient }) {
+  // const endpoint = await getHttpEndpoint({ network: 'mainnet' })
+  // const client = new TonClient({ endpoint })
+  // const { client } = useTonClient()
   const gameAddress = Address.parse('EQCG2m0OX_VTLLtJk8o_kDaIcWQeTxrQ_NyG312Gz29tfRNw')
   const game = new Game(gameAddress)
-  const gameContract = client.open(game)
-
-  const value = await gameContract.getConfig()
-  return value
+  // const gameContract = client.open(game)
+  if (client) {
+    const gameContract = client.open(game)
+    const value = await gameContract.getConfig()
+    console.log('value : ', value.results)
+    return value
+  }
 }
 
 // main()
--- src/hooks/update-result.ts
+++ src/hooks/update-result.ts
@@ -1,46 +1,60 @@
 /** @format */
 
 import { getHttpEndpoint } from '@orbs-network/ton-access'
-import { TonClient, Address, WalletContractV4 } from 'ton'
+import { TonClient, Address, WalletContractV4, OpenedContract, Sender, Contract } from 'ton'
 import { mnemonicToWalletKey } from 'ton-crypto'
-import Game, { configToCell } from '../libs/game'
+import { GAME_CONTRACT_ADDRESS } from '../const/game'
+import Game, { configToCell } from '../contract/game'
+import { useAsyncInitialize } from './useAsyncInitialize'
+import { useTonClient } from './useTonClient'
 import { useTonConnect } from './useTonConnect'
 
-export async function updateMain() {
-  const endpoint = await getHttpEndpoint({ network: 'mainnet' })
-  const client = new TonClient({ endpoint })
-  // const { sender, connected } = useTonConnect()
-  console.log('1. update start!------------')
+type UpdateProps = {
+  sender: Sender
+  wallet: string
+  client: TonClient
+}
+export async function updateMain({ sender, wallet, client }: UpdateProps) {
+  // const endpoint = await getHttpEndpoint({ network: 'mainnet' })
+  // const client = new TonClient({ endpoint })
+  // const { wallet, sender } = useTonConnect()
+  // console.log(client)
+
+  // const { client } = useTonClient()
   // console.log(sender, connected)
   // 지갑연결이 됐는지, 어떤 주소가 연결이 됐는지
-  const mnemonic = 'trend type fringe express phone custom phrase prevent gauge method anxiety vapor drastic attack stomach void culture priority fire nominee spoil orange exile gain'
-  const key = await mnemonicToWalletKey(mnemonic.split(' '))
-  const wallet = WalletContractV4.create({
-    publicKey: key.publicKey,
-    workchain: 0,
-  })
-  console.log('publickKey : ', key.publicKey)
-  console.log('publickKey : ', key.secretKey)
-  const walletContract = client.open(wallet)
-  const walletSender = walletContract.sender(key.secretKey)
-  //
 
-  const seqno = await walletContract.getSeqno()
+  // const mnemonic = ''
+  // const key = await mnemonicToWalletKey(mnemonic.split(' '))
+  // const wallet = WalletContractV4.create({
+  //   publicKey: key.publicKey,
+  //   workchain: 0,
+  // })
 
-  const gameAddress = Address.parse('EQCG2m0OX_VTLLtJk8o_kDaIcWQeTxrQ_NyG312Gz29tfRNw')
+  // const walletContract = client.open(wallet)
+  // const walletSender = walletContract.sender(key.secretKey)
+  const gameAddress = Address.parse(GAME_CONTRACT_ADDRESS)
   const game = new Game(gameAddress)
   const gameContract = client.open(game)
 
-  const config = await gameContract.getConfig() //sender가 없어도 호출할 수 있음
-  config.results.push() //{ address: gameAddress, win: 1, lose: 0, tie: 0 }
-  await gameContract.sendUpdateResult(walletSender, configToCell(config))
+  // const walletContract = useAsyncInitialize(async () => {
+  //   if (!gameContract || !client) return
+  //   const ticWalletAddress = Address.parse(wallet!)
+  //   return client!.open(new TicWallet(ticWalletAddress)) as OpenedContract<TicWallet>
+  // }, [gameContract, client])
 
-  let currentSeqno = seqno
-  while (currentSeqno == seqno) {
-    console.log('waiting for transaction to confirm...')
-    await sleep(1500)
-    currentSeqno = await walletContract.getSeqno()
-  }
+  // const seqno = await walletContract!.getSeqno()
+
+  const config = await gameContract.getConfig() //sender가 없어도 호출할 수 있음
+  config.results.push({ address: Address.parse(wallet), win: 2, lose: 0, tie: 0 }) //{ address: gameAddress, win: 1, lose: 0, tie: 0 }
+  await gameContract.sendUpdateResult(sender, configToCell(config))
+
+  // let currentSeqno = seqno
+  // while (currentSeqno == seqno) {
+  //   console.log('waiting for transaction to confirm...')
+  //   await sleep(1500)
+  //   currentSeqno = await walletContract.getSeqno()
+  // }
   console.log('transaction confirmed!')
 }
 
