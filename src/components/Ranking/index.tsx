7cdd5ef2bf987dd413d30bf89905eb5c8a18d43e
jjiyeon <ejy1105@gmail.com>
2023-03-18 01:15:01 +0900

    feat: 랭킹 컴포넌트 추가

--- src/components/Ranking/index.tsx
+++ src/components/Ranking/index.tsx
@@ -1,11 +1,30 @@
 import * as UI from '../style'
+import { Result } from '../../contract/game'
 
-const Ranking = () => {
+const toFiveOfResult = (result: Result[]) => {
+  const sortedResults = result
+    .sort((a, b) => {
+      if (b.win > a.win) return 1
+      if (b.win < a.win) return -1
+      if (b.win === a.win) {
+        return b.tie - a.tie
+      }
+      return 0
+    })
+    .slice(0, 5)
+  return sortedResults
+}
+const Ranking = ({ config, onCloseClick }: { config: Result[]; onCloseClick: () => void }) => {
   return (
     <UI.ModalContainer>
       <UI.Wrapper>
         <UI.WinnerText>Ranking</UI.WinnerText>
-        {/* <UI.ResetButton onClick={() => onResetClick()}>Replay</UI.ResetButton> */}
+        <UI.RankingButton onClick={onCloseClick}>X</UI.RankingButton>
+        <UI.RankingUsers>
+          {toFiveOfResult(config).map((val, idx) => (
+            <li key={idx}>{`${idx + 1}.st ${val.address.toRawString().slice(0, 12)}...${val.address.toRawString().slice(-4)}`}</li>
+          ))}
+        </UI.RankingUsers>
       </UI.Wrapper>
     </UI.ModalContainer>
   )
