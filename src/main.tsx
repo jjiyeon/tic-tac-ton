347e2881280fd4b350c00d5b75715189544b559d
jjiyeon <ejy1105@gmail.com>
2023-03-20 10:51:01 +0900

    fix:

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import { TonConnectUIProvider } from '@tonconnect/ui-react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 // this manifest is used temporarily for development purposes
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
-// const manifestUrl = '/tictacton-manifest.json'
+// const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+const manifestUrl = '/tictacton-manifest.json'
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
